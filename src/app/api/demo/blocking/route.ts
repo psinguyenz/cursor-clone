c780927ff2156238458a90ba5c6d50d63b191cd4
Hoahong2002 <psinguyenz@gmail.com>
2026-01-19 22:26:12 +0700

    04: background jobs

---
+++ src/app/api/demo/blocking/route.ts
@@ -0,0 +1,12 @@
+// POST localhost:3000/api/demo/blocking
+import { google } from '@ai-sdk/google';
+import { generateText } from 'ai';
+
+export async function POST() {
+    const response = await generateText({
+        model: google('gemini-2.5-flash'),
+        prompt: 'Write a vegetarian lasagna recipe for 4 people.',
+    });
+
+    return Response.json({ response })
+};
